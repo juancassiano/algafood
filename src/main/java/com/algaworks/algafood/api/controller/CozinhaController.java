b60fd15eb748d5b0cb949c2d5a0317d237fa9c4e
Juan Cassiano <juancassiano@hotmail.com>
2023-01-02 18:18:14 -0300

    Refactor code for save and remove method

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
@@ -79,17 +81,15 @@ public class CozinhaController {
     @DeleteMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
         try {
-            Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-            if (cozinha != null) {
-                cozinhaRepository.remover(cozinha);
-
+                cadastroCozinha.excluir(cozinhaId);
 //        return ResponseEntity.noContent().build();
                 return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-            }
 
+            }catch (EntidadeNaoEncontradaException e){
 //        return ResponseEntity.notFound().build();
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        } catch (DataIntegrityViolationException e) {
+
+        } catch (EntidadeEmUsoException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
     }
--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -11,5 +11,5 @@ public interface CozinhaRepository {
     List<Cozinha> listar();
     Cozinha buscar(Long id);
     Cozinha salvar(Cozinha cozinha);
-    void remover(Cozinha cozinha);
+    void remover(Long Id);
 }
