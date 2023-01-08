1dd33f94317678e5874029000b89861065d870c0
Juan Cassiano <juancassiano@hotmail.com>
2023-01-08 17:34:49 -0300

    feature: Mapping @OneToMany

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -22,5 +24,9 @@ public class Cozinha {
 //    @JsonProperty("titulo") muda o nome da representação
     @Column(nullable = false)
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<Restaurante>();
 
 }
