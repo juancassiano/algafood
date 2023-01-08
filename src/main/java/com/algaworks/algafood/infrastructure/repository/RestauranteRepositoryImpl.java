051e8c38aabe53207d9add63323ac8b4449d5f2b
Juan Cassiano <juancassiano@hotmail.com>
2023-01-07 23:50:49 -0300

    feature: Introducing of JQPL dynamic consult

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.algaworks.algafood.infrastructure.repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -18,13 +21,32 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 
-        var jqpl = "from Restaurante where nome like :nome" +
-                " and taxaFrete between :taxaInicial and :taxaFinal";
+        var jqpl = new StringBuilder();
+        jqpl.append("from Restaurante where 0 = 0 ");
 
-        return manager.createQuery(jqpl, Restaurante.class)
-                .setParameter("nome","%" + nome + "%")
-                .setParameter("taxaInicial",taxaFreteInicial)
-                .setParameter("taxaFinal",taxaFreteFinal)
-                .getResultList();
+        var parametros = new HashMap<String, Object>();
+
+        if(StringUtils.hasLength(nome)){
+            jqpl.append("and nome like :nome ");
+            parametros.put("nome", "%" + nome + "%");
+        }
+
+        if(taxaFreteInicial!=null){
+            jqpl.append("and taxaFrete >= :taxaInicial ");
+            parametros.put("taxaInicial", taxaFreteInicial);
+        }
+
+        if(taxaFreteInicial!=null){
+            jqpl.append("and taxaFrete <= :taxaFinal ");
+            parametros.put("taxaFinal", taxaFreteFinal);
+        }
+
+
+        TypedQuery<Restaurante> query = manager.createQuery(jqpl.toString(), Restaurante.class);
+
+        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+
+        return query.getResultList();
+
     }
 }
