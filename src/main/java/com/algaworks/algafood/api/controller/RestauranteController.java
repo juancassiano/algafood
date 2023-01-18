4da5e8ad3defb0a6c2e698859029a272e8cf5dfe
Juan Cassiano <juancassiano@hotmail.com>
2023-01-18 19:55:22 -0300

    refactor: create NegocioException to launch new generals exceptions.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
@@ -40,7 +41,11 @@ public class RestauranteController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Restaurante adicionar(@RequestBody Restaurante restaurante){
-        return cadastroRestauranteService.salvar(restaurante);
+        try{
+            return cadastroRestauranteService.salvar(restaurante);
+        }catch(EntidadeNaoEncontradaException e){
+            throw new NegocioException(e.getMessage());
+        }
     }
 
     @PutMapping("/{restauranteId}")
@@ -49,7 +54,11 @@ public class RestauranteController {
 
         BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formaspagamento",
                         "endereco", "dataCadastro", "produtos");
-                return cadastroRestauranteService.salvar(restauranteAtual);
+        try{
+            return cadastroRestauranteService.salvar(restauranteAtual);
+        }catch (EntidadeNaoEncontradaException e){
+            throw new NegocioException(e.getMessage());
+        }
 
     }
     @PatchMapping("/{restauranteId}")
