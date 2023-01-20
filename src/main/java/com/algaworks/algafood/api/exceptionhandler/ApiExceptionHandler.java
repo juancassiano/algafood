1c06e48ff44c48fcac77a5ec4629479c51aabb76
Juan Cassiano <juancassiano@hotmail.com>
2023-01-20 11:18:56 -0300

    feature: Create exceptionhandler for unsuported media type.

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,5 +28,14 @@ public class ApiExceptionHandler {
                 .mensagem(e.getMessage()).build();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+    }
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem("O tipo de mídia não é aceito.").build();
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(problema);
     }
 }
