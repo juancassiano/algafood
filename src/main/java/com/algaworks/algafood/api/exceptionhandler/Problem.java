b49d382c3abcee8d796ba6bb43246bb48eeab81a
Juan Cassiano <juancassiano@hotmail.com>
2023-01-24 18:47:09 -0300

    feature: Handle with MethodArgumentNotValidException, using a List of problem

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
@@ -18,4 +19,13 @@ public class Problem {
 
     private String userMessage;
     private LocalDateTime timestamp;
+
+    private List<Field> fields;
+
+    @Getter
+    @Builder
+    public static class Field {
+        private String name;
+        private String userMessage;
+    }
 }
