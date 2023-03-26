396021f7722c7de3c52b97966f1a34c59d4a73f9
Juan Cassiano <juancassiano@hotmail.com>
2023-03-26 04:56:29 -0300

    :recycle: feat: Lidando com emails duplicados

--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -8,14 +8,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
 @Service
 public class CadastroUsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+
     @Transactional
     public Usuario salvar(Usuario usuario){
+        usuarioRepository.detach(usuario);
+
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if(usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)){
+            throw new NegocioException(
+                    String.format("Já existe um usuário cadastrado com esse email %s", usuario.getEmail())
+            );
+        }
         return usuarioRepository.save(usuario);
     }
 
