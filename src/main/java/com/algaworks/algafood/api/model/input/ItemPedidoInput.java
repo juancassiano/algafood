c8cce73b890c3bb27548060446a01080b4ab0fec
juancassiano <juancassiano@hotmail.com>
2023-05-01 13:35:54 -0300

    :sparkles: feat: Alterando Status do Pedido

--- src/main/java/com/algaworks/algafood/api/model/input/ItemPedidoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/ItemPedidoInput.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.api.model.input;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
