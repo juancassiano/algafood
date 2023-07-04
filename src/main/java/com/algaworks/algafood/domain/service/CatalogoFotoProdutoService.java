09d2f0bba695a8076346d84a1b413e2e16333cf7
juancassiano <juancassiano@hotmail.com>
2023-07-04 11:24:22 -0300

    :sparkles: feat: salvando em storage

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -3,9 +3,11 @@ package com.algaworks.algafood.domain.service;
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.algaworks.algafood.domain.service.FotoStorageService.NovaFoto;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.io.InputStream;
 import java.util.Optional;
 
 @Service
@@ -14,15 +16,32 @@ public class CatalogoFotoProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private FotoStorageService fotoStorageService;
+
     @Transactional
-    public FotoProduto salvar(FotoProduto foto){
+    public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo){
         Long restauranteId = foto.getRestauranteId();
         Long produtoId = foto.getProduto().getId();
+        String nomeNovoArquivo = fotoStorageService.gerarNomeArquivo(foto.getNomeArquivo());
+
         Optional<FotoProduto> fotoExistente = produtoRepository.findFotoById(restauranteId, produtoId);
 
         if(fotoExistente.isPresent()){
             produtoRepository.delete(fotoExistente.get());
         }
-        return produtoRepository.save(foto);
+
+        foto.setNomeArquivo(nomeNovoArquivo);
+        foto = produtoRepository.save(foto);
+        produtoRepository.flush();
+
+        NovaFoto novaFoto = NovaFoto.builder()
+                .nomeArquivo(foto.getNomeArquivo())
+                .inputStream(dadosArquivo)
+                .build();
+
+        fotoStorageService.armazenar(novaFoto);
+
+        return foto;
     }
 }
