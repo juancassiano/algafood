ceabe46552ef9e3b7578ee1257f9c77e6c6216ef
juancassiano <juancassiano@hotmail.com>
2023-10-03 13:31:36 -0300

    feat: Configurando ApiInfo

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.algaworks.algafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,9 +21,18 @@ public class SpringFoxConfig {
                 .select()
                     .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 //                    .paths(PathSelectors.ant("/restaurantes/*"))
-                    .build();
+                    .build()
+                .apiInfo(apiInfo());
     }
 
 
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Algafood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1.0")
+                .contact(new Contact("Juan Cassiano", "https://github.com/juancassiano", "juancassiano@hotmail.com"))
+                .build();
+    }
 
 }
