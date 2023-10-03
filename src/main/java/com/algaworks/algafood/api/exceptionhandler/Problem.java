087dcd6078aae8652863b0f2cad2333b95f436e7
juancassiano <juancassiano@hotmail.com>
2023-10-03 16:07:24 -0300

    feat: Descreve os status de Erros do endpoint

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -1,31 +1,41 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
+@ApiModel("Problema")
 @Getter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Problem {
-
+    @ApiModelProperty(example = "400",position = 1)
     private Integer status;
+    @ApiModelProperty(example = "https://algafood.com.br/erro-negocio",position = 10)
     private String type;
+    @ApiModelProperty(example = "Violação de regra de negócio",position = 20)
     private String title;
+    @ApiModelProperty(example = "Não existe um cadastro de estado com codigo 1",position = 30)
     private String detail;
-
+    @ApiModelProperty(example = "Não existe um cadastro de estado com codigo 1",position = 35)
     private String userMessage;
+    @ApiModelProperty(example = "2021-11-14T00:12:13",position = 40)
     private OffsetDateTime timestamp;
-
+    @ApiModelProperty(value="Objetos ou campos que geraram o erro",position = 50)
     private List<Object> objects;
 
+    @ApiModel("ObjetoProblema")
     @Getter
     @Builder
     public static class Object {
+        @ApiModelProperty(example = "Estado")
         private String name;
+        @ApiModelProperty(example = "Estado não encontrado")
         private String userMessage;
     }
 }
--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,20 +1,30 @@
 package com.algaworks.algafood.core.openapi;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import springfox.documentation.builders.*;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+    TypeResolver typeResolver = new TypeResolver();
+
 
     @Bean
     public Docket apiDocket(){
@@ -22,8 +32,15 @@ public class SpringFoxConfig {
                 .select()
                     .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 //                    .paths(PathSelectors.ant("/restaurantes/*"))
+
                     .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, globalGetResponseMessages())
+                .globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+                .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+                .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
                 .apiInfo(apiInfo())
+                .additionalModels(typeResolver.resolve(Problem.class))
                 .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
 
@@ -35,6 +52,81 @@ public class SpringFoxConfig {
                 .version("1.0")
                 .contact(new Contact("Juan Cassiano", "https://github.com/juancassiano", "juancassiano@hotmail.com"))
                 .build();
+    }
+
+
+    private List<Response> globalDeleteResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(comoString(HttpStatus.INTERNAL_SERVER_ERROR))
+                        .description("Erro interno do Servidor")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(problemBuilder())
+                        .build(),
+                new ResponseBuilder()
+                        .code(comoString(HttpStatus.BAD_REQUEST))
+                        .description("Requisição inválida (erro do cliente)")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(problemBuilder())
+                        .build()
+        );
+    }
+
+
+    private List<Response> globalPostPutResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(comoString(HttpStatus.INTERNAL_SERVER_ERROR))
+                        .description("Erro interno do Servidor")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(problemBuilder())
+                        .build(),
+                new ResponseBuilder()
+                        .code(comoString(HttpStatus.NOT_ACCEPTABLE))
+                        .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(comoString(HttpStatus.BAD_REQUEST))
+                        .description("Requisição inválida (erro do cliente)")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(problemBuilder())
+                        .build(),
+                new ResponseBuilder()
+                        .code(comoString(HttpStatus.UNSUPPORTED_MEDIA_TYPE))
+                        .description("Requisição recusada porque o corpo está em um formato não suportado")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(problemBuilder())
+                        .build()
+        );
+    }
+
+    private List<Response> globalGetResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(comoString(HttpStatus.INTERNAL_SERVER_ERROR))
+                        .description("Erro interno do Servidor")
+                        .representation( MediaType.APPLICATION_JSON )
+                        .apply(problemBuilder())
+                        .build(),
+                new ResponseBuilder()
+                        .code(comoString(HttpStatus.NOT_ACCEPTABLE))
+                        .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                        .build()
+        );
+    }
+
+    private String comoString(HttpStatus httpStatus) {
+        return String.valueOf(httpStatus.value());
+    }
+
+    private Consumer<RepresentationBuilder> problemBuilder() {
+        return r -> r.model(m -> m.name("Problema")
+                .referenceModel(
+                        ref -> ref.key(
+                                k -> k.qualifiedModelName(
+                                        q -> q.name("Problema")
+                                                .namespace("com.algaworks.algafood.api.exceptionhandler")
+                                ))));
     }
 
 }
