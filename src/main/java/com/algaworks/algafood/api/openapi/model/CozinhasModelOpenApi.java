441aa72eb940e80f4a30e8ecf706c8d08e1bdb75
juancassiano <juancassiano@hotmail.com>
2023-10-06 10:25:30 -0300

    feat: substituição de Page em documentação

--- src/main/java/com/algaworks/algafood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/model/CozinhasModelOpenApi.java
@@ -2,26 +2,12 @@ package com.algaworks.algafood.api.openapi.model;
 
 import com.algaworks.algafood.api.model.CozinhaModel;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @ApiModel("CozinhasModel")
 @Getter
 @Setter
-public class CozinhasModelOpenApi {
-    private List<CozinhaModel> content;
-    @ApiModelProperty(example = "10", value = "Quantidade de registros por página")
-    private Long size;
+public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel> {
 
-    @ApiModelProperty(example = "50", value = "Total de registros")
-    private Long totalElements;
-
-    @ApiModelProperty(example = "5", value = "Total de páginas")
-    private Long totalPages;
-
-    @ApiModelProperty(example = "0", value = "Número da página (começa em 0)")
-    private Long number;
 }
