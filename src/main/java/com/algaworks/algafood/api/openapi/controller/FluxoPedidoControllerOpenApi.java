abc2a7adb83beddff1ee22385bf59f6bc59133c5
juancassiano <juancassiano@hotmail.com>
2023-10-18 16:35:01 -0300

    feat: Descrevendo documentação de endpoints de fluxo de pedido

--- src/main/java/com/algaworks/algafood/api/openapi/controller/FluxoPedidoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/FluxoPedidoControllerOpenApi.java
@@ -11,20 +11,20 @@ public interface FluxoPedidoControllerOpenApi {
             @ApiResponse(code = 204, message = "Pedido confirmado com sucesso"),
             @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
     })
-    void confirmar(@ApiParam(value = "Código do pedido", example = "7507075d-b84f-4362-ba30-b39f2835f49f") String codigoPedido);
+    void confirmar(@ApiParam(value = "Código do pedido", example = "7507075d-b84f-4362-ba30-b39f2835f49f", required = true) String codigoPedido);
 
     @ApiOperation("Cancelar Pedido")
     @ApiResponses({
             @ApiResponse(code = 204, message = "Pedido cancelado com sucesso"),
             @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
     })
-    void cancelar(@ApiParam(value = "Código do pedido", example = "7507075d-b84f-4362-ba30-b39f2835f49f") String codigoPedido);
+    void cancelar(@ApiParam(value = "Código do pedido", example = "7507075d-b84f-4362-ba30-b39f2835f49f", required = true) String codigoPedido);
 
     @ApiOperation("Entregar Pedido")
     @ApiResponses({
             @ApiResponse(code = 204, message = "Pedido entregue com sucesso"),
             @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
     })
-    void entregar(@ApiParam(value = "Código do pedido", example = "7507075d-b84f-4362-ba30-b39f2835f49f") String codigoPedido);
+    void entregar(@ApiParam(value = "Código do pedido", example = "7507075d-b84f-4362-ba30-b39f2835f49f", required = true) String codigoPedido);
 
 }
