25ab68c63b1c3bb203ab3f632081b704989f99b8
juancassiano <juancassiano@hotmail.com>
2023-10-18 16:36:23 -0300

    feat: Descrevendo documentação de associação de restaurantes com forma de pagamento

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -1,40 +1,21 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.FormaPagamentoModelAssembler;
-import com.algaworks.algafood.api.assembler.RestauranteInputDisassembler;
-import com.algaworks.algafood.api.assembler.RestauranteModelAssembler;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
-import com.algaworks.algafood.api.model.RestauranteModel;
-import com.algaworks.algafood.api.model.input.RestauranteInput;
-import com.algaworks.algafood.core.validation.ValidacaoException;
-import com.algaworks.algafood.domain.exception.CidadeNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.NegocioException;
-import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
+import com.algaworks.algafood.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.SmartValidator;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Map;
 
 @RestController()
 @RequestMapping("/restaurantes/{restauranteId}/formas-pagamento")
-public class RestauranteFormaPagamentoController {
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 
     @Autowired
     private RestauranteRepository restauranteRepository;
@@ -45,7 +26,7 @@ public class RestauranteFormaPagamentoController {
     @Autowired
     private FormaPagamentoModelAssembler formaPagamentoModelAssembler;
 
-    @GetMapping()
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<FormaPagamentoModel> listar(@PathVariable Long restauranteId){
 
         Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
---
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.FormaPagamentoModel;
+import io.swagger.annotations.*;
+
+import java.util.List;
+
+@Api(tags = "Restaurantes")
+public interface RestauranteFormaPagamentoControllerOpenApi {
+
+    @ApiOperation("Lista as formas de pagamento associadas ao restaurantes")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
+    })
+    List<FormaPagamentoModel> listar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId);
+
+    @ApiOperation("Dessasocia a forma de pagamento do restaurantes")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
+            @ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
+    })
+    void desassociar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId,
+                     @ApiParam(value = "ID da forma de pagamento", example = "1", required = true)
+                     Long formaPagamentoId);
+
+    @ApiOperation("Associa a forma de pagamento do restaurantes")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
+            @ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
+    })
+    void associar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId,
+                  @ApiParam(value = "ID da forma de pagamento", example = "1", required = true)
+                  Long formaPagamentoId);
+}
