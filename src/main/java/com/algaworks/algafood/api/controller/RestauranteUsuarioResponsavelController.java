4c92f738dd1666ebadfc64daaa9a7bdee0185c99
juancassiano <juancassiano@hotmail.com>
2023-10-25 19:23:51 -0300

    feat:Montando modelo de representação com RepresentationModelAssembler em Estado e Usuario

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -10,8 +10,8 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/responsaveis")
@@ -27,7 +27,10 @@ public class RestauranteUsuarioResponsavelController implements RestauranteUsuar
     public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId){
         Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
 
-        return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis());
+        return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis())
+                .removeLinks()
+                .add(linkTo(methodOn(RestauranteUsuarioResponsavelController.class)
+                .listar(restauranteId)).withSelfRel());
     }
 
     @DeleteMapping("/{usuarioId}")
