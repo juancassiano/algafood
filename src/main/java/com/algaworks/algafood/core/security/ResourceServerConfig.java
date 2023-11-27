3d79c81185cf043c55cd61d860062a2e938f7350
juancassiano <juancassiano@hotmail.com>
2023-11-27 08:31:12 -0300

    feat: Configurando o Resource Server para JWT assinado com chave simétrica

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -9,6 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +31,16 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                     .cors()
                 .and()
                     .oauth2ResourceServer()
-                    .opaqueToken()
+                    .jwt()
         ;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        SecretKey secretKey = new SecretKeySpec("fdsnfjasnfah8oh3faij3aofh3ofih8fhfuehfhafhweifj380".getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 }
